8632e05d37c181a30fdeb546767c092a54bb54e1
sevbesau <seppe@sevbesau.xyz>
2022-03-27 14:09:05 +0200

    cleanup

--- .config/wal/templates/colors-wal-slock.h
+++
@@ -1,5 +0,0 @@
-static const char *colorname[NUMCOLS] = {{
-    [ICON] = "{color1}",   /* icon color */
-    [INPUT] = "{color2}",  /* during input */
-    [FAILED] = "{color3}", /* wrong password */
-}};
